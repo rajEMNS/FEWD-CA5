c5579e07369b9d4285c125f132e0c994f358c1a8
NKRTECH <141981269+NKRTECH@users.noreply.github.com>
2023-12-20 07:03:17 +0530

    Update Registration.jsx

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -1,125 +1,3 @@
-// import { useState, useEffect } from "react";
-// import "./registration.css";
-// import { useNavigate } from "react-router-dom";
-
-
-// function Registration() {
-//   const initialValues = { username: "", email: "", password: "", confirmPassword: "" };
-//   const [formValues, setFormValues] = useState(initialValues);
-//   const [formErrors, setFormErrors] = useState({});
-//   const [isSubmit, setIsSubmit] = useState(false);
-//   const navigate = useNavigate();
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormValues({ ...formValues, [name]: value });
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     setFormErrors(validate(formValues));
-//     setIsSubmit(true);
-//   };
-
-//   useEffect(() => {
-//     console.log(formErrors);
-//     if (Object.keys(formErrors).length === 0 && isSubmit) {
-//       console.log(formValues);
-//       navigate("/", { state: { successMessage: "Registration successful!" } });}
-//   }, [formErrors]);
-//   const validate = (values) => {
-//     const errors = {};
-//     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-//     if (!values.username) {
-//       errors.username = "Username is required!";
-//     }
-//     if (!values.email) {
-//       errors.email = "Email is required!";
-//     } else if (!regex.test(values.email)) {
-//       errors.email = "This is not a valid email format!";
-//     }
-//     if (!values.password) {
-//       errors.password = "Password is required";
-//     } else if (values.password.length < 10) {
-//       errors.password = "Password must be more than 10 characters";
-//     } else if (values.password.length > 15) {
-//       errors.password = "Password cannot exceed more than 10 characters";
-//     }
-//     if (!values.confirmPassword) {
-//       errors.confirmPassword = "Please confirm your password";
-//     } else if (values.confirmPassword !== values.password) {
-//       errors.confirmPassword = "Passwords do not match";
-//     }
-//     return errors;
-//   };
-
-// return (
-//   <div>
-//     {Object.keys(formErrors).length === 0 && isSubmit ? (
-//       <div>Signed in successfully</div>
-//     ) : (
-//       <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
-//     )}
-
-//     <form onSubmit={handleSubmit}>
-//       <h1>Login Form</h1>
-//       <div></div>
-//       <div>
-//         <div>
-//           <label>Username</label>
-//           <input
-//             type="text"
-//             name="username"
-//             placeholder="Username"
-//             value={formValues.username}
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <p>{formErrors.username}</p>
-//         <div>
-//           <label>Email</label>
-//           <input
-//             type="text"
-//             name="email"
-//             placeholder="Email"
-//             value={formValues.email}
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <p>{formErrors.email}</p>
-//         <div>
-//           <label>Password</label>
-//           <input
-//             type="password"
-//             name="password"
-//             placeholder="Password"
-//             value={formValues.password}
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <p>{formErrors.password}</p>
-//         <div>
-//           <label>Confirm Password</label>
-//           <input
-//             type="password"
-//             name="confirmPassword"
-//             placeholder="Confirm Password"
-//             value={formValues.confirmPassword}
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <p>{formErrors.confirmPassword}</p>
-//         <button>Submit</button>
-//       </div>
-//     </form>
-//   </div>
-// );
-// }
-
-// export default Registration;
-
-// Registration.js
-
 import React, { useState, useEffect } from "react";
 import "./registration.css";
 import { useNavigate } from "react-router-dom";
@@ -155,6 +33,10 @@ function Registration() {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
     if (!values.username) {
       errors.username = "Username is required!";
+    } else if (values.username.length < 3) {
+      errors.username = "Username must be more than 3 characters";
+    } else if (values.username.length > 30) {
+      errors.username = "Username cannot exceed more than 30 characters";
     }
     if (!values.email) {
       errors.email = "Email is required!";
